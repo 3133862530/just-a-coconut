0a734bb5e04f3369863226796ab4eb6f8e9e3005
yiliiii <147075114+3133862530@users.noreply.github.com>
2024-04-13 16:34:57 +0800

    Update 源.c
    文件与链表形式

--- 源.c
+++ 源.c
@@ -1,121 +1,199 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include<stdbool.h>
+#include<stdlib.h>
 
-#include"statement.h"
+void input_passenger(void);
+void check_passenger(void);
+
+struct Passenger //乘客个人信息
+{
+    char name[20];//名字
+    char idcard[19];//18位身份证
+    char contact_information[12];//联系方式
+    char password[20];//密码
+
+    char Registration_number;//注册编号 UID
+    char fancy[50];//旅客偏好
+    char seat[2][3];//座位 例：13 A
+    struct Passenger* next;
+};
+
+int appendPassengerNode(struct Passenger** headRef);
+void printList(struct Passenger* head);
+void freeList(struct Passenger* head);
+void writeListToFile(struct Passenger** head, const char* filename);
+void readListFromFile(struct Passenger** head, const char* filename);
+void writeListToNode(struct Passenger** head, int number);
 
 
-Flight NEU[20];//NEU航空的二十个航班
-Administrator NEUer[5];//五个管理员
-Passenger FBI[500];//五百个乘客
 int main(void)
 {
-	
+    int i;
+
+	while(1){
+		printf("                 @@@@@***************************欢迎使用航班订票系统***************************@@@@@\n");
+		printf("                 ###                              1 录 入 乘 客 信 息                             ###\n");
+                printf("                 @@@                              2 查 看 航 班 信 息                             @@@\n");
+		printf("                 #####!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#####\n");
+		printf("请选择: ");
+		scanf("%d",&i);
+		switch(i){
+			case 1:input_passenger();break;
+            case 2:check_passenger(); break;
+            default:break;
+		}
+	}
+
+
+
+
+    
+    
+    
+}
+void input_passenger()
+{
+    struct Passenger** WNEUer = NULL;
+    int number = appendPassengerNode(&WNEUer);//创建链表
+    writeListToNode(&WNEUer, number);//将乘客数据写入链表
+    writeListToFile(&WNEUer, "乘客数据.txt");//将链表写入文件
+    system("cls");
+    //freeList(WNEUer);//释放过程还有问题 暂未解决
+}
+void check_passenger()
+{
+    system("cls");
+    struct Passenger** RNEUer = NULL;
+    readListFromFile(&RNEUer, "乘客数据.txt");
+    printList(RNEUer);
+    //freeList(RNEUer);
 }
 
 
 
-void initializeAirplane(int FlightNumber)// 初始化飞机座位状态
+void writeListToNode(struct Passenger** head, int number)
 {
-    for (int i = 0; i < MAX_ROWS; i++)
+    struct Passenger* current = *head;
+    
+    for (int i = 0; i < number; i++)
     {
-        for (int j = 0; j < MAX_COLS; j++)
-        {
-            NEU[FlightNumber].seats[i][j].booked = false;
-        }
+        scanf("%s", current->name);
+        scanf("%s", current->idcard);
+        scanf("%s", current->contact_information);
+        scanf("%s", current->password);
+        scanf("%s", current->Registration_number);
+        scanf("%s", current->fancy);
+        scanf("%s %s", current->seat[0], current->seat[1]);
+        current = current->next;
     }
+    
 }
 
-void displayAirplaneStatus(int FlightNumber) // 显示飞机座位状态
-{
-    printf("Airplane Seating Arrangement:\n");
-    for (int i = 0; i < MAX_ROWS; i++)
+int appendPassengerNode(struct Passenger** headRef)//简而言之 
+{//输入链表的头指针 和要加入的结构体即可 即可创建n个链表
+    printf("输入添加乘客数量：");
+    int n;
+    scanf("%d", &n);
+    for (int i =0; i < n; i++)
     {
-        for (int j = 0; j < MAX_COLS; j++)
+        struct Passenger* newPassenger = (struct Passenger*)malloc(sizeof(struct Passenger));
+        newPassenger->next = NULL;
+        if (*headRef == NULL)//检查传递进来的链表头指针是否为零 若是 
         {
-            if (NEU[FlightNumber].seats[i][j].booked)
-            {
-                printf("[X] "); // X表示已预定
-            }
-            else
-            {
-                printf("[ ] "); // 空格表示可预定
-            }
+            *headRef = newPassenger;//则建立第一个节点
+            continue;//终止
         }
+        struct Passenger* current = *headRef;
+        while (current->next != NULL)//一路畅通拉下去到头
+        {
+            current = current->next;
+        }
+        current->next = newPassenger;//拉到头了 加到尾巴上
+    }
+
+    return n;
+}
+
+void printList(struct Passenger* head) 
+{
+    struct Passenger* current = head;
+    while (current != NULL) 
+    {
+        printf("Name: %s\n", current->name);
+        printf("ID Card: %s\n", current->idcard);
+        printf("Contact Information: %s\n", current->contact_information);
+        printf("Password: %s\n", current->password);
+        printf("Fancy: %s\n", current->fancy);
+        printf("Seat: %s %s\n", current->seat[0], current->seat[1]);
         printf("\n");
+        current = current->next;
     }
 }
-
-
-
-
-
-void bookSeat(int row, int col, int FlightNumber) // 预订座位
+void freeList(struct Passenger* head)
 {
-    if (!NEU[FlightNumber].seats[row][col].booked)
+    struct Passenger* current = head;
+    while (current != NULL) 
     {
-        NEU[FlightNumber].seats[row][col].booked = true;
-        printf("Seat at Row %d, Column %d booked successfully.\n", row, col);
+        struct Passenger* temp = current;
+        current = current->next;
+        free(temp->name);
+        free(temp->idcard);
+        free(temp->contact_information);
+        free(temp->password);
+        free(temp->fancy);
+        free(temp);
     }
-    else
+}
+
+void readListFromFile(struct Passenger** head, const char* filename)
+{//输入一个头指针 然后把文件数据都读到链表中
+    FILE* file = fopen(filename, "rb");
+    if (file == NULL)
     {
-        printf("Seat at Row %d, Column %d is already booked.\n", row, col);
+        printf("Error opening file.\n");
+        return;
     }
-}
-
-void cancelSeat(int row, int col, int FlightNumber) // 取消预订座位
-{
-    if (NEU[FlightNumber].seats[row][col].booked)
+    struct Passenger temp;
+    while (fread(&temp, sizeof(struct Passenger), 1, file) != 0)
     {
-        NEU[FlightNumber].seats[row][col].booked = false;
-        printf("Seat at Row %d, Column %d canceled successfully.\n", row, col);
+        struct Passenger* newPassenger = (struct Passenger*)malloc(sizeof(struct Passenger));
+        if (newPassenger == NULL)
+        {
+            printf("Memory allocation failed.\n");
+            //fclose(file);
+            return;
+        }
+
+        *newPassenger = temp;
+        newPassenger->next = NULL;
+        if (*head == NULL)
+        {
+            *head = newPassenger;
+        }
+        else
+        {
+            struct Passenger* current = *head;
+            while (current->next != NULL)
+            {
+                current = current->next;
+            }
+            current->next = newPassenger;
+        }
+        
     }
-    else
+    fclose(file);
+}
+
+    
+void writeListToFile(struct Passenger** head, const char* filename)
+{
+    //输入一个链表 把链表数据都写到文件中
+    FILE* file = fopen(filename, "wb");
+    struct Passenger* temp = *head;
+    while (temp != NULL)//没写完就一直写
     {
-        printf("Seat at Row %d, Column %d is not booked.\n", row, col);
+        fwrite(temp, sizeof(struct Passenger), 1, file);
+        temp = temp->next;
     }
-}
-
-
-//这里调用了航班信息与个人信息 
-// 因为题目要求包含对两种信息查询与修改的需求 功能都写上 用不上没事
-//最后应该都要加上将结构体中的数据读写到文件中 这个还不会 之后再加上 先把现在能写的函数写好
-void Register(Passenger* wyl, Flight NEU[])//初次注册信息 直接把结构体拉进来加数据
-{
-    //注册，录入自己的信息，包括姓名、联系电话等；
-    //内容格式参考Passenger部分进行填充
-}
-void ReRegister(Passenger* wyl, Flight NEU[])//修改个人信息 直接把结构体拉进来改数据
-{
-    //对自己的信息进行修改
-}
-void Inquire(Passenger* wyl, Flight NEU[])//查询及预定航班
-{
-    //对各种信息（包括航班预订信息）进行查询，包括简单、组合、模糊查询，
-    // 例如查询某天某个航班是否有空余座位、查询自己的所有预订情况等
-}
-void Rank(Passenger* wyl, Flight NEU[])//信息排序
-{
-    //对各种信息（包括航班预订信息）进行排序，包括按单一属性、按多属性排序等，
-    // 尽可能对查询结果进行多种排序（例如对查询出来的航班按照价格进行升序排序）
-
-}
-void Count(Passenger* wyl, Flight NEU[])//信息统计
-{
-    //对各种信息（包括航班预订信息）进行统计，
-    // 包括按单一属性、按多属性统计、预设统计、按条件统计等
-    // （例如统计自己总的乘机次数、自己每年的乘机花费等）
-}
-
-
-void Ad_regulation(Administrator NEUer[],Flight NEU[])//管理员对航班信息的管理
-{
-    //对航线、航班、仓位等级等信息进行管理，包括增加、修改、删除等。
-}
-void SystemMaintenance(Administrator NEUer[], Flight NEU[])//维护 这里应该要用到文件的读写部分 我还没理清楚 不急
-{
-    //对自己的密码进行维护、数据备份、数据恢复
-}
-void displayMenu()
-{
-    //菜单暂定
+    fclose(file);
 }
